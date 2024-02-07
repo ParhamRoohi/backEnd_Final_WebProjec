0c4c713046f6cb0b124a3b402e6060371262dfba
Parham.Roohi <par81hamroohi@gmail.com>
2024-02-07 14:45:14 +0330

    Third Commit

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ const postgres = require('postgres');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 const port = 4000;
 
-PGHOST='ep-flat-hill-a5zkc9o9.us-east-2.aws.neon.tech'
-PGDATABASE='finalProject'
-PGUSER='par81hamroohi@gmail.com'
-PGPASSWORD='v9lSfezO6cCg'
-ENDPOINT_ID='ep-flat-hill-a5zkc9o9'
-
-// PGUSER = decodeURIComponent(PGUSER);
+let PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID;
+PGHOST = 'ep-flat-hill-a5zkc9o9.us-east-2.aws.neon.tech'
+PGDATABASE = 'finalProject'
+PGUSER = 'par81hamroohi@gmail.com'
+PGPASSWORD = 'v9lSfezO6cCg'
+ENDPOINT_ID = 'ep-flat-hill-a5zkc9o9'
 
 const sql = postgres({
   host: PGHOST,
@@ -29,11 +28,41 @@ const sql = postgres({
 
 
 
-app.get("/products", (request, response) => {
-  sql`SELECT * FROM products`.then((result) => {
-    response.send(result);
-  });
+app.get("/products", async(req, res) => {
+  // sql `SELECT * FROM products`.then((result) => {
+    const products = [
+      {id:"1", brand: "Hello", name: "Par09154093785ham˚-˚" ,description:"3hrfuyf",amount:134, storage:12, price:5634652},
+      // {id:"2", text: "Hello", createdBy: "Hediye" },
+    ];
+    res.send(products);
+  // });
 });
+
+// app.delete("/products/:id", (req, res) => {
+//   const productsId = req.params.id;
+
+//   sql`DELETE FROM tasks WHERE id = ${productsId}`
+//     .then(() => {
+//       res.send({ message: `Product with ID ${productsId} has been deleted.` });
+//     })
+//     .catch((error) => {
+//       res.status(500).send("Error deleting product.");
+//     });
+// });
+// app.post("/products", (req, res) => {
+//   const {brand, name, description, amount, storage, price } = req.body;
+//   sql`INSERT INTO products (productName, description) VALUES ( ${brand}, ${name}, ${description}, ${amount},${storage},${price})`.then((result) => {
+//     res.send({
+//       message: `New product was created. `
+//     })
+//     res.send(result);
+//   })
+//     .catch((error) => {
+//       res.status(500).send("Error creating product.");
+//     });
+
+// })
+
 
 app.listen(port, () =>
   console.log(` My App listening at http://localhost:${port}`)
