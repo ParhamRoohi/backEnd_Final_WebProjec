af63ce4541e003d053ab4325fce201fd735f9f6f
Parham.Roohi <par81hamroohi@gmail.com>
2024-02-15 01:32:42 +0330

    edit get request

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const port = 4000;
+app.use((_, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+const port = 3000;
 
 let PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID;
 PGHOST = 'ep-flat-hill-a5zkc9o9.us-east-2.aws.neon.tech'
@@ -27,41 +34,74 @@ const sql = postgres({
 });
 
 
+// get all products
+// app.get("/products", async (req, res) => {
+//   sql`SELECT * FROM products`.then((result) => {
+//     res.send(result);
+//   });
+// });
 
-app.get("/products", async(req, res) => {
-  // sql `SELECT * FROM products`.then((result) => {
-    const products = [
-      {id:"1", brand: "Hello", name: "Par09154093785ham˚-˚" ,description:"3hrfuyf",amount:134, storage:12, price:5634652},
-      // {id:"2", text: "Hello", createdBy: "Hediye" },
-    ];
-    res.send(products);
-  // });
-});
+// search method by name
+// app.get("/products", (req, res) => {
+//   const productsName = req.params.name;
 
-// app.delete("/products/:id", (req, res) => {
-//   const productsId = req.params.id;
-
-//   sql`DELETE FROM tasks WHERE id = ${productsId}`
-//     .then(() => {
-//       res.send({ message: `Product with ID ${productsId} has been deleted.` });
+//   sql`SELECT * FROM products WHERE name = ${productsName}`
+//     .then((result) => {
+//       res.send(result);
 //     })
 //     .catch((error) => {
-//       res.status(500).send("Error deleting product.");
+//       res.status(404).send("Error to find this products");
 //     });
 // });
-// app.post("/products", (req, res) => {
-//   const {brand, name, description, amount, storage, price } = req.body;
-//   sql`INSERT INTO products (productName, description) VALUES ( ${brand}, ${name}, ${description}, ${amount},${storage},${price})`.then((result) => {
-//     res.send({
-//       message: `New product was created. `
-//     })
-//     res.send(result);
-//   })
-//     .catch((error) => {
-//       res.status(500).send("Error creating product.");
-//     });
 
-// })
+// filter by brand
+app.get("/products", async (req, res) => {
+  try {
+    const brandQuery = await req.query.brand;
+     let  product = sql`SELECT * FROM products`;
+    let filtereProducts = [];
+    if (brandQuery) {
+      filtereProducts.push(sql`brand LIKE '%${brandQuery}%'`)
+      res.send(filtereProducts);
+    }
+  }
+  // else {
+  //   sql`SELECT * FROM products`.then((result) => {
+  //     res.send(result);
+  //   });
+  // }
+  catch (err) {
+    res.send(err.message)
+  }
+});
+
+// delete products
+app.delete("/products/:id", (req, res) => {
+  const productsId = req.params.id;
+
+  sql`DELETE FROM products WHERE id = ${productsId}`
+    .then(() => {
+      res.send({ message: `Product with ID ${productsId} has been deleted.` });
+    })
+    .catch((error) => {
+      res.status(500).send("Error deleting product.");
+    });
+});
+
+//create new prouducts
+app.post("/products", (req, res) => {
+  const { brand, name, description, amount, storage, price } = req.body;
+  sql`INSERT INTO products (brand, productName, description, amount, storage, price) VALUES ( ${brand}, ${name}, ${description}, ${amount},${storage},${price})`.then((result) => {
+    res.send({
+      message: `New product was created. `
+    })
+    res.send(result);
+  })
+    .catch((error) => {
+      res.status(500).send("Error creating product.");
+    });
+
+})
 
 
 app.listen(port, () =>
