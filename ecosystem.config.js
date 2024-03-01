848c0868be5fac738f5f2693bf5d6acc49c3a78b
Parham.Roohi <par81hamroohi@gmail.com>
2024-03-01 18:39:00 +0330

    ecosystem.config

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [{
+        name: '<backEnd_Final_WebProject>',
+        script: '<index.js>',
+        env: {
+            NODE_ENV: 'development',
+        }
+    }],
+};
